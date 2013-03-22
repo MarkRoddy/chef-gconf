031e983e146172cdb355ed8726bc6dbab212efe2
Mark Roddy <markroddy@gmail.com>
2013-03-22 00:44:32 +0000

    use dbus_launch at cmd line instead of dbus_session env variable which may not exist at run time

--- providers/default.rb
+++ providers/default.rb
@@ -1,26 +1,33 @@
+
+# Get the current value for the specified setting
+def get_current_gconf_value(user, confkey)
+  output = `sudo -u #{user} dbus-launch gconftool-2 --get #{confkey}`
+  return output.strip()
+end
+
 action :set do
-  dbus_session = "$(grep -v \"^#\" $HOME/.dbus/session-bus/`cat /var/lib/dbus/machine-id`-0)"
-  cmd = "env #{dbus_session} "
-  cmd += "gconftool-2 --set #{new_resource.key} --type #{new_resource.attr_type} '#{new_resource.attr_val}'"
+  cmd = "dbus-launch gconftool-2 --set #{new_resource.key} --type #{new_resource.attr_type} '#{new_resource.attr_val}'"
   if 'list' == new_resource.attr_type then
     cmd += " --list-type #{new_resource.list_type}"
   end
-  cmd_name = "Setting gconf key '#{new_resource.key}' to '#{new_resource.attr_val}'"
-  execute cmd_name do
-    command cmd
-    # environment {} Use this for dbus at some point
-    user new_resource.user
+  current_value = get_current_gconf_value(new_resource.user, new_resource.key)
+  if current_value != new_resource.attr_val then
+    cmd_name = "Setting gconf key #{new_resource.key} from #{current_value} to #{new_resource.attr_val}"
+    execute cmd_name do
+      command cmd
+      # environment {} Use this for dbus at some point
+      user new_resource.user
+    end
   end
+
 end
 
 action :delete do
-  dbus_session = "$(grep -v \"^#\" $HOME/.dbus/session-bus/`cat /var/lib/dbus/machine-id`-0)"
-  cmd = "env #{dbus_session} "
   if new_resource.recursive then
-    cmd += "gconftool-2 --recursive-unset #{new_resource.key}"
+    cmd = "dbus-launch gconftool-2 --recursive-unset #{new_resource.key}"
     cmd_name = "Recursively unsetting gconf key '#{new_resource.key}'"
   else
-    cmd += "gconftool-2 --unset #{new_resource.key}"
+    cmd = "dbus-launch gconftool-2 --unset #{new_resource.key}"
     cmd_name = "Unsetting gconf key '#{new_resource.key}'"
   end
   execute cmd_name do
